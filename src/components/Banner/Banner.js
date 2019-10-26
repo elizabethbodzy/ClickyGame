c54dec1d4f355c063798100f48db62d8bce7452b
Elizabeth Bodzy <bodze087@newschool.edu>
2019-10-25 22:57:22 -0500

    file folder structure and started frontend css and js files

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => (
+    <div className="banner text-center d-flex align-items-center justify-content-center">
+      <p className="m-0">Click on a character in Seinfeld to earn points, but don't click on a character more than once!</p>
+    </div>
+  );
+  
+  export default Banner;
---
+++ src/images.js
@@ -0,0 +1,25 @@
+import elaine from "./images/elaine.png";
+import ellen from "./images/ellen.png";
+import estelle from "./images/estelle.png";
+import frank from "./images/frank.png";
+import george from "./images/george.png";
+import jerry from "./images/jerry.png";
+import kramer from "./images/kramer.png";
+import morty from "./images/morty.png";
+import nana from "./images/nana.png";
+import newman from "./images/newman.png";
+
+const images = [
+    elaine,
+    ellen,
+    estelle,
+    frank,
+    george,
+    jerry,
+    kramer,
+    morty,
+    nana,
+    newman
+];
+
+export default images;
