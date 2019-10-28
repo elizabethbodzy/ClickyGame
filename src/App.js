f13d79f38657a86368ecc1e0fb2c6871d778def4
Elizabeth Bodzy <bodze087@newschool.edu>
2019-10-27 20:19:09 -0500

    console logging new image array when clicked

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Banner from './components/Banner';
 import Character from './components/Character';
 // import images from '../src/images';
 import images from './images.json';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 // const App = () => (
@@ -27,8 +27,27 @@ class App extends Component {
     images,
     score: 0,
     highScore: 0,
-    navMessage: "Click on an image to begin!"
+    navMessage: "Click on an image to begin!",
+    clicked: [],
   };
+
+  shuffle = array => {
+    array.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < array.length; i++) {
+      array[i].id = i;
+    }
+  }
+
+  componentDidMount() {
+    var newImagesArray = this.state.images;
+    this.shuffle(newImagesArray);
+    this.setState({ images: newImagesArray});
+  }
+
+  clickCount = id => {
+    var newImagesArray = this.state.images;
+    console.log(newImagesArray);
+  }
 
   render() {
     const state = this.state;
@@ -44,10 +63,11 @@ class App extends Component {
       <Wrapper>
         {this.state.images.map(image => (
           <Character 
-            clickCount = {this.clickCount}
             id = {image.id}
             key = {image.id}
             image = {image.image}
+            clicked = {image.clicked}
+            onClick = {() => this.clickCount(image.id)}
           />
         ))}
         
